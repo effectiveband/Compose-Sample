ba46a2a0162f868ded58711198b43bf29be5703a
Max Turchin <maxim.turchin@effective.band>
2022-04-17 19:41:45 +0600

    [+] Add news language switch based on system language

--- news-api/src/main/kotlin/band/effective/headlines/compose/news_api/di/NewsApiDependencies.kt
+++ news-api/src/main/kotlin/band/effective/headlines/compose/news_api/di/NewsApiDependencies.kt
@@ -1,8 +1,7 @@
 package band.effective.headlines.compose.news_api.di
 
+import band.effective.headlines.compose.core.di.CommonDependencies
 import band.effective.headlines.compose.core.di.ComponentDependencies
 import retrofit2.Retrofit
 
-interface NewsApiDependencies : ComponentDependencies {
-    //val retrofit: Retrofit
-}
+interface NewsApiDependencies : CommonDependencies
